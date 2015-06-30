94649e19cb4875cdce8ae713d3727a1fc10cf11d
mdolivo <mdolivo@users.noreply.github.com>
2015-06-30 15:42:43 -0430

    Cambio de coma por punto

--- PruebaRandon.js
+++ PruebaRandon.js
@@ -1,6 +1,6 @@
 var numero = Math.random();
 if(numero <= 0.5){
-	console.log('\n' + numero + 'Menor que 0,5 \n');
+	console.log('\n' + numero + 'Menor que 0.5 \n');
 }else{
-	console.log('\n' + numero + 'Mayor que 0,5 \n');
+	console.log('\n' + numero + 'Mayor que 0.5 \n');
 }
